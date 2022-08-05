90abe36b5eb51baeab25a2a266ac45204eaf420f
Taenor <39013240+Taenor@users.noreply.github.com>
2022-08-05 10:23:09 +0200

    Update define_DS.sh

--- Augmented_DS/define_DS.sh
+++ Augmented_DS/define_DS.sh
@@ -18,7 +18,7 @@ do
         j=$(expr $tot*$i/100 | bc)
         echo $a
         echo $j
-        if [[ $a == gun_shot || $a == door_slamming || $a == fireworks || $a == clapping || $a == glass_breaking || $a == microphone_tap || $a == snapping ]]
+        if [[ $a == gun_shot || $a == door_slamming || $a == fireworks || $a == clapping || $a == glass_breaking || $a == microphone_tap || $a == snapping || $a == car_horn || $a == crash || $a == dog_bark || $a == pothole || $a == scream || $a == tire ]]
         then
           find ./ -path "./foreground/$a/*.wav" | shuf -n $j > ./listes/list_$a.txt
           find ./ -path "./foreground/$a/*.wav" | shuf -n $j >> ./list.txt
